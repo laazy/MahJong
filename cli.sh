1574740b8d480e36abea26f39a63cbdea41c1336
laazy <616408874@qq.com>
2021-12-14 22:41:32 +0800

    :heavy_plus_sign: add coverage for unittest

---
+++ cli.sh
@@ -0,0 +1,16 @@
+# /usr/bin/bash
+set -e
+OLD_PWD=$PWD
+cd `dirname $0`
+
+unittest(){
+    pipenv run python -m unittest discover -s ./tests/ -t .
+}
+
+coverage(){
+    pipenv run coverage run -m unittest discover -s ./tests/ -t .
+}
+
+coverage
+
+cd $OLD_PWD
