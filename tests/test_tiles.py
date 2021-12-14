3f9b869f8f1724b37b62ecd2c071b07667814a71
laazy <616408874@qq.com>
2021-12-14 23:17:30 +0800

    :white_check_mark: 适配基础牌堆

--- tests/test_tiles.py
+++ tests/test_tiles.py
@@ -4,18 +4,19 @@ from mahjong.tiles import Tiles, NoTileError
 
 
 class TilesTestCase(unittest.TestCase):
+    TOTAL_COUNT = 136
     def test_capicity(self):
         tiles = Tiles()
-        self.assertEqual(tiles.capicity, 144)
+        self.assertEqual(tiles.capicity, self.TOTAL_COUNT)
 
     def test_size(self):
         tiles = Tiles()
-        self.assertEqual(tiles.size, 144)
+        self.assertEqual(tiles.size, self.TOTAL_COUNT)
 
     def test_draw(self):
         tiles = Tiles()
-        for i in range(144):
-            self.assertEqual(tiles.size, 144 - i)
+        for i in range(self.TOTAL_COUNT):
+            self.assertEqual(tiles.size, self.TOTAL_COUNT - i)
             tiles.draw()
         self.assertEqual(tiles.size, 0)
         with self.assertRaises(NoTileError):
@@ -35,6 +36,6 @@ class TilesTestCase(unittest.TestCase):
         self.assertEqual(tiles.see(), tiles.see())
         self.assertEqual(tiles.see(0), tiles.see(0))
         self.assertEqual(tiles.see(55), tiles.see(55))
-        self.assertEqual(tiles.see(143), tiles.see(143))
+        self.assertEqual(tiles.see(self.TOTAL_COUNT - 1), tiles.see(self.TOTAL_COUNT - 1))
         with self.assertRaises(NoTileError):
-            tiles.see(144)
+            tiles.see(self.TOTAL_COUNT)
